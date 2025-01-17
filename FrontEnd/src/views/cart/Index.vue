b159c2f1403047466f138ebf44f02ad41bb2000d
Satria Yudha <satriayuda2017@gmail.com>
2025-01-17 13:15:17 +0800

    delete product from order

--- FrontEnd/src/views/cart/Index.vue
+++ FrontEnd/src/views/cart/Index.vue
@@ -7,6 +7,7 @@
         :key="item.id"
 
         :item="item"
+        v-on:remove-item="removeFromCart($event)"
         />
         <h3 id="total-price">Total: Rp{{ totalPrice }}</h3>
         <button id="checkout-button">Checkout</button>
@@ -27,6 +28,16 @@ export default {
   data(){
     return {
       cartItems: []
+    }
+  },
+  methods: {
+    async removeFromCart(product) {
+      await axios.delete(`http://localhost:8000/api/orders/delete/user/1/product/${product}`)
+      let indexCart = this.cartItems.map(function(item) {
+        return item.code
+      }).indexOf(product)
+      
+      this.cartItems.splice(indexCart, 1)
     }
   },
   computed: {
